4c03d21b7998ce422dfc0f87f3decf90199ad86d
Livingstone Fultang <livingstonefultang@gmail.com>
2016-03-18 16:20:18 +0000

    Timeline stream update

--- src/Controller/Member/Timeline/Stream.php
+++ src/Controller/Member/Timeline/Stream.php
@@ -3,14 +3,17 @@
 namespace Budkit\Cms\Controller\Member\Timeline;
 
 use Budkit\Cms\Controller\Member;
+use Budkit\Cms\Model\Story;
+use ArrayObject;
 
 class Stream extends Member\Timeline {
 
 
     public function manage($format = 'html')
     {
-        echo "Display all timeline filters and show add more filters";
+        //echo "Display all timeline filters and show add more filters";
 
+        $this->view->setLayout("posts/post-labels");
 
     }
 
@@ -18,7 +21,19 @@ class Stream extends Member\Timeline {
     public function mentions($format = 'html'){
         //echo $type;
 
-        parent::index($format);
+        $this->view->setData("title", "@Mentions");
+
+        $story = $this->application->createInstance( Story::class );
+        $graph = $story->getByUserMentionInContent( $this->user->getCurrentUser() );
+
+        //die;
+
+        $this->view->setData("stories", getArrayObjectAsArray( new ArrayObject( $graph->getEdgeSet() ) ) );
+
+
+        $this->timeline();
+
+        //parent::index($format);
     }
 
 
